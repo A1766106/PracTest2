d51086f7a2068ca2be07da0a0cd2d1b78082db1b
Max Bruce <maxbruce@student-10-201-05-195.wireless.adelaide.edu.au>
2022-08-26 15:13:28 +0930

    cheer

--- function-4-1.cpp
+++ function-4-1.cpp
@@ -4,3 +4,28 @@
 #include <string>
 
 using namespace std;
+
+void decrement();
+int get_val();
+void increment();
+ModCounter(int init,int bound);
+
+void decrement()
+{
+    current_val = current_val - 1;
+    return;
+}
+int get_val()
+{
+    return current_val;
+}
+void increment()
+{
+    current_val = current_val + 1;
+    return;
+}
+ModCounter(int init,int bound)
+{
+    current_val = init;
+    boundary = bound;
+}
